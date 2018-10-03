6afd8dcec4c3166d89be974b59e4cb3f456e17cf
Alec Nelson <alecnelson24@gmail.com>
2018-10-03 14:38:25 -0400

    Added Alec(tm) Input Header

--- Analysis.Team.Test.Script.9.27.18.R
+++ Analysis.Team.Test.Script.9.27.18.R
@@ -1,17 +1,32 @@
-## igraph.test
-#10/3/18
+#Data Processing for ICON 8002
+# 10/2/18
+
+############################
+basedirectory <- "C:\\Users\\ahn11803\\Documents\\GitHub\\ICON8002_SNA"
+inputdata_path <- "C:\\Users\\ahn11803\\Documents\\GitHub\\ICON8002_SNA\\Data"
+
+vertex_datapath<-"vertex_test.csv"
+edge_datapath<-"attribute_test1.csv"
+
+setwd(inputdata_path)
+
+list.of.packages <- c("igraph")
+
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)){install.packages(new.packages)} 
+
+#Load all packages
+lapply(list.of.packages, require, character.only = TRUE)
+########################################################
+########################################################
 
 ## input data structure
 ## 2 data sheets (csv files), 1 for vertices, 1 for connections/edges
 ## this seems to be common practice
 ## attributes can be numeric and characters, continuous and categorical
 
-
-getwd()
-setwd("/Users/alecnelson/Documents/UGA_Files/Fall_2018/ICON_8002")
-
-vertex_test<- read.csv("vertex_test.csv",header=T)
-edge_test<- read.csv("attribute_test1.csv",header=T)
+vertex_test<- read.csv(vertex_datapath,header=T)
+edge_test<- read.csv(edge_datapath,header=T)
 
 str(vertex_test)
 summary(vertex_test)
@@ -57,3 +72,4 @@ vcount(icon.graph)
 edge_density(icon.graph)
 
 icon.graph[]
+
